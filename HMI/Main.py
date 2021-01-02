668feec1b2caf5847f40128d07e6f31c8a61ac06
Benedict Baumann <benedict.baumann@8sense.com>
2021-01-02 20:53:15 +0100

    pictures Indis

--- HMI/Main.py
+++ HMI/Main.py
@@ -35,7 +35,9 @@ root.config(background="#000000")  # Hintrgrundfarbe des Fensters
 leftFrame = Frame(root, width=200, height=400, background="#000000")
 leftFrame.grid(row=0, column=0, padx=10, pady=3)
 
-
+# Image import
+IndLeft = PhotoImage(file = "~/repos/HMI_LIAM/HMI/venv/Pictures/IndLeft.png")
+IndRight = PhotoImage(file = "~/repos/HMI_LIAM/HMI/venv/Pictures/IndRight.png")
 
 # imageEx = PhotoImage(file='200x200')
 # Label(leftFrame, image=imageEx).grid(row=2, column=0, padx=10, pady=3)
@@ -262,13 +264,13 @@ LightON.grid(row=0, column=2, padx=10, pady=3)
 Warning_Lights = Button(buttonFrame, text="Warning Lights", bg="#FFF000", width=15, height=5, command=WarningLight)
 Warning_Lights.grid(row=1, column=1, padx=10, pady=3)
 
-Indicator_Left = Button(buttonFrame, text="Indicator Left", bg="#FFF000", width=15, height=5, command=BlinkLeft)
+Indicator_Left = Button(buttonFrame, image = IndLeft, text="Indicator Left", bg="#FFF000", width=15, height=5, command=BlinkLeft)
 Indicator_Left.grid(row=2, column=0, padx=10, pady=3)
 
 Indicators_OFF = Button(buttonFrame, text="Indicator OFF", bg="#FFFF00", width=15, height=5, command=blinkOff)
 Indicators_OFF.grid(row=2, column=1, padx=10, pady=3)
 
-Indicator_Right = Button(buttonFrame, text="Indicator Right", bg="#FFF000", width=15, height=5, command=BlinkRight)
+Indicator_Right = Button(buttonFrame, image = IndRight, text="Indicator Right", bg="#FFF000", width=15, height=5, command=BlinkRight)
 Indicator_Right.grid(row=2, column=2, padx=10, pady=3)
 
 
