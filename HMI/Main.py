8f17c70d5d8df39382952198e133b3411329d942
Benedict Baumann <benedict.baumann@8sense.com>
2021-01-02 17:36:06 +0100

    TestFrames

--- HMI/Main.py
+++ HMI/Main.py
@@ -25,7 +25,7 @@ BlinkArray1 = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 
 BlinkArray2 = [64, 65, 66, 72, 73, 74, 80, 81, 82, 88, 89, 90, 96, 97, 98, 104, 105, 106, 112, 113, 114, 120, 121, 122]
 Chairstate = 0  # if chair turned 180° state = 1
 
-global switch   #switch for Indicators
+global switch  # switch for Indicators
 
 root = Tk()  # Fenster erstellen
 root.wm_title("Raspberry Pi GUI")  # Fenster Titel
@@ -49,6 +49,8 @@ rightFrame.grid(row=0, column=1, padx=10, pady=3)
 E1 = Entry(rightFrame, width=50)
 E1.grid(row=0, column=0, padx=10, pady=3)
 
+def OverviewPage ():
+
 
 def LightOFF():
     global switch
@@ -58,6 +60,7 @@ def LightOFF():
         strip19.setPixelColorRGB(i, 0, 0, 0)
         strip18.show()
         strip19.show()
+
 
 def StandingLight():
     print(E1.get())
@@ -191,6 +194,7 @@ def BlinkLeft():
         strip19.show()
         time.sleep(0.6)
 
+
 def WarningLight():
     global switch
     switch = False  # to shutdown all other blinkers
@@ -230,31 +234,50 @@ def blinkOff():
     switch = False
 
 
-buttonFrame = Frame(rightFrame)
-buttonFrame.grid(row=1, column=0, padx=10, pady=3)
+def Frame1():
+    # Light Buttons/Frame
+    buttonFrame = Frame(rightFrame)
+    buttonFrame.grid(row=1, column=0, padx=10, pady=3)
 
-LightOFF = Button(buttonFrame, text="OFF", bg="#FF0000", width=15, height =10, command=LightOFF)
-LightOFF.grid(row=0, column=0, padx=10, pady=3)
+    LightOFF = Button(buttonFrame, text="OFF", bg="#FF0000", width=15, height=10, command=LightOFF)
+    LightOFF.grid(row=0, column=0, padx=10, pady=3)
 
-THRO = Button(buttonFrame, text="Standing light", bg="#FF0000", width=15, height =10, command=StandingLight)
-THRO.grid(row=0, column=1, padx=10, pady=3)
+    THRO = Button(buttonFrame, text="Standing light", bg="#FF0000", width=15, height=10, command=StandingLight)
+    THRO.grid(row=0, column=1, padx=10, pady=3)
 
-LightON = Button(buttonFrame, text="LIGHT", bg="#FF0000", width=15, height =10, command=LightON)
-LightON.grid(row=0, column=2, padx=10, pady=3)
+    LightON = Button(buttonFrame, text="LIGHT", bg="#FF0000", width=15, height=10, command=LightON)
+    LightON.grid(row=0, column=2, padx=10, pady=3)
 
-Warning_Lights = Button(buttonFrame, text="Warning Lights", bg="#FFF000", width=15, height =10, command=WarningLight)
-Warning_Lights.grid(row=1, column=1, padx=10, pady=3)
+    Warning_Lights = Button(buttonFrame, text="Warning Lights", bg="#FFF000", width=15, height=10, command=WarningLight)
+    Warning_Lights.grid(row=1, column=1, padx=10, pady=3)
 
-Indicator_Left = Button(buttonFrame, text="Indicator Left", bg="#FFF000", width=15, height =10, command=BlinkLeft)
-Indicator_Left.grid(row=2, column=0, padx=10, pady=3)
+    Indicator_Left = Button(buttonFrame, text="Indicator Left", bg="#FFF000", width=15, height=10, command=BlinkLeft)
+    Indicator_Left.grid(row=2, column=0, padx=10, pady=3)
 
-Indicators_OFF = Button(buttonFrame, text="Indicator OFF", bg="#FFFF00", width=15, height =10, command=blinkOff)
-Indicators_OFF.grid(row=2, column=1, padx=10, pady=3)
+    Indicators_OFF = Button(buttonFrame, text="Indicator OFF", bg="#FFFF00", width=15, height=10, command=blinkOff)
+    Indicators_OFF.grid(row=2, column=1, padx=10, pady=3)
 
-Indicator_Right = Button(buttonFrame, text="Indicator Right", bg="#FFF000", width=15, height =10, command=BlinkRight)
-Indicator_Right.grid(row=2, column=2, padx=10, pady=3)
+    Indicator_Right = Button(buttonFrame, text="Indicator Right", bg="#FFF000", width=15, height=10, command=BlinkRight)
+    Indicator_Right.grid(row=2, column=2, padx=10, pady=3)
 
-Slider = Scale(rightFrame, from_=0, to=100, resolution=0.1, orient=HORIZONTAL, length=400)
-Slider.grid(row=2, column=0, padx=10, pady=3)
+    Slider = Scale(rightFrame, from_=0, to=100, resolution=0.1, orient=HORIZONTAL, length=400)
+    Slider.grid(row=2, column=0, padx=10, pady=3)
+
+def Frame2():
+    TestFrame = Frame(rightFrame, TExt = "1234")
+    TestFrame.grid(row=1, column=0, padx=10, pady=3)
+
+# Menubuttons
+
+menuFrame = Frame(leftFrame)
+menuFrame.grid(row=1, column=0, padx=10, pady=3)
+
+Overview = Button(menuFrame, text="Overview", bg="#FF0000", width=15, height=10, command=Frame2)
+Overview.grid(row=0, column=0, padx=10, pady=3)
+
+Lights = Button(menuFrame, text="Lights", bg="#FF0000", width=15, height=10, command=Frame1)
+Lights.grid(row=1, column=0, padx=10, pady=3)
+
+
 
 root.mainloop()  # GUI wird upgedatet. Danach keine Elemente setzen
